0eb74c065ca189d1036eb483240bbecc9e184d59
Thxrindu <tharindunirmal996@gmail.com>
2022-06-04 18:24:31 +0530

    Errors fixed

--- models/supervisor-models/researchTopic.model.js
+++ models/supervisor-models/researchTopic.model.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const researchTopicSchema = new Schema({
+  topic: { type: String },
   groupName: { type: String, required: true, unique: true },
-  topic: { type: String, unique: true },
   state: { type: String },
-  csState: { type: String },
+  // csState: { type: String },
   // researchField: { type: String }
   //marks need to add
 }, {
